a4dae6ea25e7c29b45e07b044567948a073369b9
Nathaniel Kotzrenko <n.kotzrenko@gmail.com>
2019-10-14 19:03:26 +0300

    Added callbacks for feedback about database connection status

--- server/src/mongo/Connection.ts
+++ server/src/mongo/Connection.ts
@@ -1,5 +1,5 @@
 import { Board, register as registerBoard } from "./models/Board";
-import { connect, Connection, Model } from "mongoose";
+import { connect, connection, Model } from "mongoose";
 import { User, register as registerUser } from "./models/User";
 
 interface Models {
@@ -9,9 +9,15 @@ interface Models {
 
 connect("mongodb://ec2-3-9-169-24.eu-west-2.compute.amazonaws.com:27017/guards", { useNewUrlParser: true });
 
+connection.db.on("open", () => console.log("Connected to mongo database"));
+connection.db.on("error", () => {
+    console.error("Database connection failed!");
+    process.exit(1);
+})
+
 const models: Models = {
     board: registerBoard(),
     user: registerUser()
 };
 
-export { Connection, models };
+export { connection, models };
