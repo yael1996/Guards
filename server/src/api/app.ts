389a3e066967c1ae7bb00b108dcb6fa6f65ed439
Nathaniel Kotzrenko <n.kotzrenko@gmail.com>
2019-10-14 19:11:17 +0300

    Removed redundent class when declaring the express application server

--- server/src/api/app.ts
+++ server/src/api/app.ts
@@ -1,20 +1,7 @@
 import express from "express";
 import bodyParser from "body-parser";
 
-export class App {
-  public app: express.Application;
+const app = express();
 
-  constructor() {
-    this.app = express();
-    this.config();
-  }
-
-  private config(): void {
-    // this._app.use(express.static("../src"));
-
-    // support application/json
-    this.app.use(bodyParser.json());
-    //support application/x-www-form-urlencoded post data
-    this.app.use(bodyParser.urlencoded({ extended: false }));
-  }
-}
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
