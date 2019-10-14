0b85d5d4cc26fb6a54ccc76a23a5da16cce1a1d1
Nathaniel Kotzrenko <n.kotzrenko@gmail.com>
2019-10-14 18:59:03 +0300

    Added User model into the DAL type

--- server/src/mongo/Connection.ts
+++ server/src/mongo/Connection.ts
@@ -1,14 +1,17 @@
 import { Board, register as registerBoard } from "./models/Board";
 import { connect, Connection, Model } from "mongoose";
+import { User, register as registerUser } from "./models/User";
 
 interface Models {
-    board: Model<Board>
+    board: Model<Board>,
+    user: Model<User>
 }
 
 connect("mongodb://ec2-3-9-169-24.eu-west-2.compute.amazonaws.com:27017/guards", { useNewUrlParser: true });
 
 const models: Models = {
-    board: registerBoard()
+    board: registerBoard(),
+    user: registerUser()
 };
 
 export { Connection, models };
