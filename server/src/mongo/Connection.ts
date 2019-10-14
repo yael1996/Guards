d1c52f4a3fab8756c1994748d11cd988a2206bd6
Nathaniel Kotzrenko <n.kotzrenko@gmail.com>
2019-10-14 17:37:29 +0300

    Created a connection module

---
+++ server/src/mongo/Connection.ts
@@ -0,0 +1,14 @@
+import { Board, register as registerBoard } from "./models/Board";
+import { connect, Connection, Model } from "mongoose";
+
+interface Models {
+    board: Model<Board>
+}
+
+connect("mongodb://ec2-3-9-169-24.eu-west-2.compute.amazonaws.com:27017/guards", { useNewUrlParser: true })
+
+const models: Models = {
+    board: registerBoard()
+}
+
+export { Connection, models }
