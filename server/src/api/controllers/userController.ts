25b102cb2e9d37cb9a87128c809494d2fcfc619a
Nathaniel Kotzrenko <n.kotzrenko@gmail.com>
2019-10-21 17:16:03 +0300

    Added ; as standard dictates it and converted the userController.ts module to use safeAsync utility

--- server/src/api/controllers/userController.ts
+++ server/src/api/controllers/userController.ts
@@ -1,47 +1,23 @@
 import { Router } from "express";
-import { models } from "../../mongo/connection"
+import { models } from "../../mongo/connection";
+import { safeAsync } from "../../utiles/safeAsync";
 
 const router = Router();
 
-router.get("/", async (req, res) => {
-  try {
-    const boards = await models.user.find();
-    res.status(200).end(boards);
-  } catch (error) {
-    res.status(500).end(error);
-  }
-});
-router.get("/:id", async (req, res) => {
-  try {
-    const board = await models.user.findById(req.params.id);
-    res.status(200).end(board);
-  } catch (error) {
-    res.status(500).end(error);
-  }
-});
-router.post("/", async (req, res) => {
-  try {
-    const board = await models.user.create(req.body);
-    res.status(200).send(board);
-  } catch (error) {
-    res.status(500).send(error);
-  }
-});
-router.delete("/:id", async (req, res) => {
-  try {
-    const board = await models.user.findByIdAndRemove(req.params.id);
-    res.status(200).send(board);
-  } catch (error) {
-    res.status(500).send(error);
-  }
-});
-router.patch("/:id", async (req, res) => {
-  try {
-    const board = await models.user.findByIdAndUpdate(req.params.id, req.body);
-    res.status(200).end(board);
-  } catch (error) {
-    res.status(500).end(error);
-  }
-});
+router.get("/", safeAsync(async (req, res) => {
+  return await models.user.find();
+}));
+router.get("/:id", safeAsync(async (req, res) => {
+  return await models.user.findById(req.params.id);
+}));
+router.post("/", safeAsync(async (req, res) => {
+  return await models.user.create(req.body);
+}));
+router.delete("/:id", safeAsync(async (req, res) => {
+  return await models.user.findByIdAndRemove(req.params.id);
+}));
+router.patch("/:id", safeAsync(async (req, res) => {
+  return await models.user.findByIdAndUpdate(req.params.id, req.body);
+}));
 
 export { router };
