d575d366c9a44f54c7be8611184cca9a6a78ea8a
Nathaniel Kotzrenko <n.kotzrenko@gmail.com>
2019-10-21 17:10:06 +0300

    Used the safeAsync utility in boardController.ts

--- server/src/api/controllers/boardController.ts
+++ server/src/api/controllers/boardController.ts
@@ -1,47 +1,23 @@
 import { Router } from "express";
 import { models } from "../../mongo/connection"
+import { safeAsync } from "../../utiles/safeAsync";
 
 const router = Router();
 
-router.get("/", async (req, res) => {
-  try {
-    const boards = await models.board.find();
-    res.status(200).end(boards);
-  } catch (error) {
-    res.status(500).end(error);
-  }
-});
-router.get("/:id", async (req, res) => {
-  try {
-    const board = await models.board.findById(req.params.id);
-    res.status(200).end(board);
-  } catch (error) {
-    res.status(500).end(error);
-  }
-});
-router.post("/", async (req, res) => {
-  try {
-    const board = await models.board.create(req.body);
-    res.status(200).send(board);
-  } catch (error) {
-    res.status(500).send(error);
-  }
-});
-router.delete("/:id", async (req, res) => {
-  try {
-    const board = await models.board.findByIdAndRemove(req.params.id);
-    res.status(200).send(board);
-  } catch (error) {
-    res.status(500).send(error);
-  }
-});
-router.patch("/:id", async (req, res) => {
-  try {
-    const board = await models.board.findByIdAndUpdate(req.params.id, req.body);
-    res.status(200).end(board);
-  } catch (error) {
-    res.status(500).end(error);
-  }
-});
+router.get("/", safeAsync(async (req, res) => {
+    return await models.board.find();
+}));
+router.get("/:id", safeAsync(async (req, res) => {
+  return await models.board.findById(req.params.id);
+}));
+router.post("/", safeAsync(async (req, res) => {
+  return await models.board.create(req.body);
+}));
+router.delete("/:id", safeAsync(async (req, res) => {
+  return await models.board.findByIdAndRemove(req.params.id);
+}));
+router.patch("/:id", safeAsync(async (req, res) => {
+  return await models.board.findByIdAndUpdate(req.params.id, req.body);
+}));
 
 export { router };
