74a6f0a82ce987f7a7a357f2780f323a3838e755
Nathaniel Kotzrenko <n.kotzrenko@gmail.com>
2019-10-21 16:37:07 +0300

    Removed the routes module and combined it into the app.ts module

--- server/src/api/app.ts
+++ server/src/api/app.ts
@@ -1,7 +1,13 @@
 import express from "express";
 import bodyParser from "body-parser";
+import { GeneticAlgorithemController } from "./controllers/geneticAlgorithemController";
+import { router as boardRouter } from "./controllers/boardController";
+import { router as userRouter } from "./controllers/userController";
 
 const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use("/board", boardRouter);
+app.use("/user", userRouter);
