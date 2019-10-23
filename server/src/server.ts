013cbf00ed992b043795a56cdd048080e664d051
Nathaniel Kotzrenko <n.kotzrenko@gmail.com>
2019-10-23 23:21:04 +0300

    Imported the server application and initialized the usage of it

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,34 +1,3 @@
-import express from "express";
-import { App } from "./api/app";
-import { Routes } from "./api/routes";
+import { app } from "./api/app";
 
-class Server {
-  private app: express.Application;
-  private routs: express.Router;
-  private port: any;
-
-  constructor() {
-    this.app = new App().app;
-    this.routs = new Routes().appRouts;
-    this.port = process.env.PORT || 3000;
-
-    this.initRouts();
-    this.listen();
-  }
-
-  private initRouts() {
-    this.app.use("/", this.routs);
-
-    // this.app.get("/", (req, res) => {
-    //   res.send("Welcome");
-    // });
-  }
-
-  private listen() {
-    this.app.listen(this.port, () => {
-      console.log("Server started on port: " + this.port);
-    });
-  }
-}
-
-const server = new Server();
+app.listen(process.env.PORT || 3000, () => {});
