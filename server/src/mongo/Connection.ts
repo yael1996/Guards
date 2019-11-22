5b0b495305406098cfdff87661ecc10261875419
Nathaniel Kotzrenko <n.kotzrenko@gmail.com>
2019-11-22 17:12:48 +0200

    Added a plugin that fixes the findOne to throw an error and not return a damn null)

--- server/src/mongo/Connection.ts
+++ server/src/mongo/Connection.ts
@@ -1,6 +1,7 @@
 import { config } from "dotenv";
+import { connect, connection, Model, set, plugin } from "mongoose";
+import findOneErrorPlugin from "mongoose-findone-or-error";
 import { Board, register as registerBoard } from "./models/board";
-import { connect, connection, Model, set } from "mongoose";
 import { User, register as registerUser } from "./models/User";
 
 interface Models {
@@ -13,6 +14,7 @@ config();
 let models: Models;
 
 set("runValidators", true);
+plugin(findOneErrorPlugin);
 
 connect(process.env.DB, { useNewUrlParser: true }).then(() => {
     connection.db.on("open", () => console.log("Connected to mongo database"));
