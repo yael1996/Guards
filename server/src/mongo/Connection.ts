ca8444c2d8250855e41d9342e4ff6273a45715d0
Nathaniel Kotzrenko <n.kotzrenko@gmail.com>
2019-11-22 12:45:05 +0200

    Fixed the server initialization

--- server/src/mongo/Connection.ts
+++ server/src/mongo/Connection.ts
@@ -7,17 +7,19 @@ interface Models {
     user: Model<User>
 }
 
-connect("mongodb://ec2-3-9-169-24.eu-west-2.compute.amazonaws.com:27017/guards", { useNewUrlParser: true });
+let models: Models;
 
-connection.db.on("open", () => console.log("Connected to mongo database"));
-connection.db.on("error", () => {
-    console.error("Database connection failed!");
-    process.exit(1);
-})
+connect("mongodb://ec2-3-9-169-24.eu-west-2.compute.amazonaws.com:27017/guards", { useNewUrlParser: true }).then(() => {
+    connection.db.on("open", () => console.log("Connected to mongo database"));
+    connection.db.on("error", () => {
+        console.error("Database connection failed!");
+        process.exit(1);
+    })
 
-const models: Models = {
-    board: registerBoard(),
-    user: registerUser()
-};
+    models = {
+        board: registerBoard(),
+        user: registerUser()
+    };
+});
 
 export { connection, models };
