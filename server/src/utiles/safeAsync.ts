7d9214a517609e79603565f9fa9ce01e37d21a21
Nathaniel Kotzrenko <n.kotzrenko@gmail.com>
2019-11-22 12:57:14 +0200

    Fixed safe async to use send instead of std node end method

--- server/src/utiles/safeAsync.ts
+++ server/src/utiles/safeAsync.ts
@@ -3,9 +3,9 @@ import { Response, Request } from "express";
 const safeAsync = (fn: (req: Request, res: Response) => Promise<any>) => async (req: Request, res: Response) => {
     try {
         const result = await fn(req, res);
-        res.status(200).end(result);
+        res.status(200).send(result);
     } catch (error) {
-        res.status(500).end(error);
+        res.status(500).send(error);
     }
 };
 
