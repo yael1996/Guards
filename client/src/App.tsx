425584cd28011a9ad80ece4fb464f137cbbfc81c
Guy <guy@alcide.io>
2019-11-26 13:27:32 +0200

    changed calendar to pure js to hold calendar dependency and wrapped in tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,31 +1,32 @@
 import React, { Component } from "react";
-import FS from './Pages/Calendar/ClientCalendar';
 
 import "./Styles/App.css";
-import "react-big-calendar/lib/css/react-big-calendar.css";
-import {DateLocalizer} from "react-big-calendar";
+import ClientCalendar from "./Pages/Calendar/CalendarComponent";
+import {CalendarActions} from "./Pages/Calendar/CalendarActions";
+// import {myFunc} from "./Pages/Calendar/CalendarActions";
+
+
 
 
 class App extends Component {
+
+    componentDidMount() {
+        this.fetchData();
+    }
+
+    fetchData = () => {
+
+        // .then(events => {
+        let a = new CalendarActions().myFunc();
+        //             // return events;
+        //             this.setState({events});
+        //         });
+    };
+
     render() {
         return (
             <div className="App" style={{height : '100%', width : '100%'}}>
-                <FS messages={date: 'Date',
-                    time: 'Time',
-                    event: 'Event',
-                    allDay: 'All Day',
-                    week: 'Week',
-                    work_week: 'Work Week',
-                    day: 'Day',
-                    month: 'Month',
-                    previous: 'Back',
-                    next: 'Next',
-                    yesterday: 'Yesterday',
-                    tomorrow: 'Tomorrow',
-                    today: 'Today',
-                    agenda: 'Agenda',
-                    noEventsInRange: 'There are no events in this range.',
-                } localizer={DateLocalizer}/>
+               <ClientCalendar />
             </div>
         );
     }
---
+++ client/src/Pages/Calendar/CalendarActions.tsx
@@ -0,0 +1,48 @@
+// import {JSONBoard} from '../../../../server/src/mongo/models/Board';
+// @ts-ignore
+// import moment from 'moment';
+// import axios from "axios";
+export class CalendarActions {
+    constructor() {}
+     myFunc = () => {
+
+        const tomorrow = new Date(new Date());
+
+        tomorrow.setDate(tomorrow.getDate() + 1);
+        return [
+            {
+                start: new Date(),
+                end: tomorrow,
+                title: "my title"
+            },
+        ];
+    }
+}
+
+// export const getUserEvents = () => {
+    // try {
+    //     // const response = await axios.get('http://127.0.0.1:3000/board');
+    //     // console.log(response.data[0]);
+    //     console.log("hErE!");
+    //     let tomorrow = moment(new Date()).add(1,"days").toDate();
+    //     return [
+    //         {
+    //             start: new Date(),
+    //             end: tomorrow,
+    //             title: "my title",//response.data[0].owner
+    //         },
+    //
+    //     ]
+    //     // const board = new JSONBoard();
+    //     // board.owner = response.data[0].owner;
+    //     // board.isOptimised = response.data[0].isOptimised;
+    //     // board.workDays = response.data[0].workDays;
+    //     //
+    //     // return board
+    //
+    // } catch (e) {
+    //     console.log("Error!!!");
+    //     console.error(e);
+    // }
+// };
+
