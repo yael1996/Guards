a29a74920d20c953e9b827e3ec96d86b3fe58199
Nathaniel Kotzrenko <n.kotzrenko@gmail.com>
2019-11-30 19:15:30 +0200

    Added the concrete router to the express applicaiton

--- server/src/api/app.ts
+++ server/src/api/app.ts
@@ -2,6 +2,7 @@ import express from "express";
 import bodyParser from "body-parser";
 import { router as boardRouter } from "./controllers/boardController";
 import { router as userRouter } from "./controllers/userController";
+import { router as concreteBoardRouter } from "./controllers/concreteBoardController";
 
 const app = express();
 
@@ -10,5 +11,6 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use("/board", boardRouter);
 app.use("/user", userRouter);
+app.use("/concreteBoard", concreteBoardRouter);
 
 export { app };
