066820691a20b7102e16b9faf32231a865fd0165
Nathaniel Kotzrenko <n.kotzrenko@gmail.com>
2019-12-02 13:31:46 +0200

    Fixed problems in DashBoard.tsx

--- client/src/Pages/DashBoard.tsx
+++ client/src/Pages/DashBoard.tsx
@@ -2,9 +2,15 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 import { State } from "../Store/store";
 import { Dispatch } from "redux";
+import { useRouteMatch } from "react-router";
+import { Header, SideMenuItem } from "../Store/types";
+import HeaderComp from "../Components/Header/Header";
+import { Switch, Route } from "react-router-dom";
+import SideMenuComp from "../Components/SideMenu/SideMenu";
 
-interface ReduxState extends State {
-
+interface ReduxState {
+    header: Header
+    menuItems: SideMenuItem[]
 }
 
 interface ReduxDispatch {
@@ -15,26 +21,32 @@ type Props = ReduxState & ReduxDispatch;
 
 class DashBoard extends Component<Props> {
     render() {
+        const { url } = useRouteMatch();
+        const { header, menuItems } = this.props;
         return (
             <div className="App" style={{height : '100%', width : '100%'}}>
-                {/* <HeaderComp data={header}/>
+                <HeaderComp data={header}/>
                 <section className="content-area">
                     <section className="main-area">
                         <Switch>
-                            <Route path="/">
-                                <ClientCalendar />
+                            <Route path={`${url}`}>
+                                test
+                                {/* <ClientCalendar /> */}
                             </Route>
                         </Switch>
                     </section>
                     <SideMenuComp data={menuItems}/>
-                </section> */}
+                </section>
             </div>
         );
     }
 }
 
 const mapStateToProps = (state: State): ReduxState => {
-    return state;
+    return {
+        header: state.header,
+        menuItems: state.menuItems
+    };
 }
 
 const mapDispatchToProps = (dispatch: Dispatch): ReduxDispatch => {
