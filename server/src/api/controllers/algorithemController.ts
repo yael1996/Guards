5771a34b46146b8a1ac277503887de7bc20ef887
yael <yael19961@gmail.com>
2019-12-06 17:37:03 +0200

    add algo route
    fix algo routs

---
+++ server/src/api/controllers/algorithemController.ts
@@ -0,0 +1,19 @@
+import { Router } from "express";
+import { GeneticAlgorithm } from "../../geneticAlgorithem/geneticAlgorithem";
+import { Month } from "../../mongo/models/concreteBoard";
+
+const router = Router();
+const algorithem = new GeneticAlgorithm();
+
+router.post("/", async (req, res) => {
+  try {
+    const bordId = req.query.bord; //req.param
+    const month = req.body.month as Month;
+    const result = await algorithem.runGeneticAlgorithm(bordId, month);
+    res.status(201).end(JSON.stringify(result));
+  } catch (error) {
+    res.status(406).end({ error: error.message });
+  }
+});
+
+export { router };
