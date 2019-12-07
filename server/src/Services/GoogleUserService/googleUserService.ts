d0ec483ac29be76b44afeccf9f3e3f12a3988366
Guy <guy@alcide.io>
2019-12-07 15:11:29 +0200

    fixed google auth

--- server/src/Services/GoogleUserService/googleUserService.ts
+++ server/src/Services/GoogleUserService/googleUserService.ts
@@ -1,5 +1,6 @@
 import { User} from "../../mongo/models/user";
 import { models } from "../../mongo/Connection";
+import {Board} from "../../mongo/models/Board";
 
 
 
@@ -17,7 +18,7 @@ class GoogleUserService extends GoogleAuthService {
     }
 
     // userLogin (user, code){
-    //     this.getAuthorizedGoogleAccountFromCode(code)
+    //     this.getGoogleAuthClient(code)
     //         .then((res)=>{
     //             return res
     //         })
@@ -26,9 +27,9 @@ class GoogleUserService extends GoogleAuthService {
     //         });
     // }
     // async getNewUser(code){
-    //     const newUser: User = await this.getAuthorizedGoogleAccountFromCode(code);
+    //     const newUser: User = await this.getGoogleAuthClient(code);
     //
-    //     // return await this.getAuthorizedGoogleAccountFromCode(code)
+    //     // return await this.getGoogleAuthClient(code)
     //     //     .then((newUser)=>{
     //     //         return new User ({
     //     //             _id: newUser.id,
@@ -50,7 +51,7 @@ class GoogleUserService extends GoogleAuthService {
     /**
      * Extract the email and id of the google account from the "code" parameter.
      */
-    async getAuthorizedGoogleAccountFromCode(code) {
+    async getGoogleAuthClient(code) {
         // get the auth "tokens" from the request
         const auth = await super.createConnection();
         const data = await auth.getToken(code); //TODO: Create a new getAuth function in GoogleAuth?
@@ -65,7 +66,7 @@ class GoogleUserService extends GoogleAuthService {
         // return this.getUserFromGoogleApi(tokens, auth);
     }
 
-    async getUserFromGoogleApi(tokens, auth): Promise<User>{
+    async getUserFromGoogleApi(auth): Promise<User>{
         const peopleApi = await this.getGooglePlusApi(auth);
         const me = await peopleApi.people.get({
                 resourceName: 'people/me',
@@ -87,16 +88,29 @@ class GoogleUserService extends GoogleAuthService {
         retUser.email = userGoogleEmail;
         // retUser.tokens = tokens; //can be saved to the user if you ever want to get their details without making them log in again
 
-
-        retUser.type = 'user';
-        retUser.boardId = "5de2e4f1157640446cbefab7";
-        // return so we can login or sign up the user
         return retUser;
+
+        // retUser.type = 'user';
+        // retUser.boardId = "5de2e4f1157640446cbefab7";
+        // return so we can login or sign up the user
     }
 
-    async isUserRegistered(userToken) {
-        const user = models.user;
-        user.find( )
-    }
+    async getUserFromDB(userEmail): Promise<User> {
+        const userInDB = await models.user.findOne({email:userEmail});
+        return (userInDB != null ? new models.user(userInDB) : null );
+        //TODO: check if null is returned when user not found - also consider if 2 users exist with same email
+            //     {
+            //         firstname: userInDB[0].firstname,
+            //         lastname: userInDB[0].lastname,
+            //         email: userInDB[0].email,
+            //         tokens: userInDB[0].tokens,
+            //         type: userInDB[0].type,
+            //         boardId: userInDB[0].boardId,
+            //         satisfiedConstraints: userInDB[0].satisfiedConstraints,
+            //         monthlyConstraints: userInDB[0].monthlyConstraints
+            //     }
+            // )
+
+        }
 }
 module.exports = GoogleUserService;
--- server/src/api/controllers/userController.ts
+++ server/src/api/controllers/userController.ts
@@ -8,42 +8,44 @@ const GoogleUserService = require("../../Services/GoogleUserService/googleUserSe
 const router = Router();
 
 
-router.post("/login", async (req, res) => {
-    //TODO: add code to check if logged in, return current page else go to google auth below
-
-    let code = req.query.code;
-    let gu = new GoogleUserService(googleConfig,defaultScope); //TODO: grab googleConfig and defaultScope from env property
-    //get new user object to save in DB
-
-})
-    .get("/register", async (req, res) => {
+router.get("/login", async (req, res) => {
         let gu = new GoogleUserService(googleConfig,defaultScope); //TODO: grab googleConfig and defaultScope from env property
         gu.getGoogleUrl()
             .then((url=>{
                 console.log(url);
-                res.redirect(url) //redirect goes to /google-auth
+                res.redirect(url) //redirect goes to /google
             }))
             .catch((err=>{
                 console.log("Failed to get GoogleURL \n %v",err)
             }))
     })
-    .get('/google-auth', async (req, res)=>{
-        //TODO: here we check if this is a registered user or not and redirect accordingly (register or login)
+    .get('/google-auth', async (req, res)=> {
+        //TODO: here we check if this is a registered user or not and redirect accordingly (register or dashboard(with ID))
 
         let code = req.query.code;
-        let gu = new GoogleUserService(googleConfig,defaultScope); //TODO: grab googleConfig and defaultScope from env property
-        gu.getAuthorizedGoogleAccountFromCode(code)
+        let gu = new GoogleUserService(googleConfig, defaultScope); //TODO: grab googleConfig and defaultScope from env property
+        let redirectUrl;
+        gu.getGoogleAuthClient(code)
             .then((auth) => {
-                if gu.isUserRegistered()
-                console.log(newUser.firstname);
-                //save user in DB
-                newUser.save(function (err, task) {
-                    if (err) res.send(err);
-                    res.json(task)
-                });
-            })
-            .catch((err) => {
-                console.log(err)
+                gu.getUserFromGoogleApi(auth).then((googleUser) => {
+                    gu.getUserFromDB(googleUser.email).then((dbUser)=> {
+                        if (dbUser != null) {
+                            //user found in DB
+                            redirectUrl = process.env.CLIENT_URL+"?user=" + Buffer.from(JSON.stringify(dbUser)).toString("base64");
+                            res.redirect(redirectUrl)
+
+                        } else {
+                            redirectUrl = process.env.CLIENT_URL+"?user=" + Buffer.from(JSON.stringify(googleUser)).toString("base64");
+                            res.redirect(redirectUrl)//redirect to site/register
+                        }
+                    });
+
+
+                })
+
+                    .catch((err) => {
+                        console.log("failed to get Google Auth, %v", err)
+                    });
             });
     });
 
@@ -98,10 +100,16 @@ router.patch("/:id", async (req, res) => {
 
 export { router };
 
-const googleConfig = {
+const googleConfig_old = {
     clientId: '1016798324260-cn24vnlh62ijn94h2sguklrelvpjntgu.apps.googleusercontent.com', // e.g. asdfghjkljhgfdsghjk.apps.googleusercontent.com
     clientSecret: 'JSzG4-1lYJS7wCVKkJcNlxNV', // e.g. _ASDFA%DFASDFASDFASD#FAD-
     redirect: 'http://localhost:3000/user/google-auth' // this must match your google api settings
+};
+
+const googleConfig = {
+    clientId: process.env.GOOGLE_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET, // e.g. _ASDFA%DFASDFASDFASD#FAD-
+    redirect: process.env.GOOGLE_REDIRECT // this must match your google api settings
 };
 
 // let ga = new GoogleAuthService(googleConfig,defaultScope);
