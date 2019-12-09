4bfa60226bce79f8689066ac2e01e4d2ae3fa878
Nathaniel Kotzrenko <n.kotzrenko@gmail.com>
2019-12-09 16:32:05 +0200

    Added action to load the correct pages by the user type

--- client/src/Store/Menu/reducer.ts
+++ client/src/Store/Menu/reducer.ts
@@ -1,16 +1,27 @@
-import { MenuState, MenuAction, SHOW_PAGE } from "./types";
+import { MenuState, MenuAction, LOAD_PAGES } from "./types";
 
 const initialState: MenuState = {
-    items: [{
-        text: "Companies",
-        page: "/dashboard/companies"
-    }]
+    items: []
 }
 
 export function reducer(state: MenuState = initialState, action: MenuAction): MenuState {
     switch (action.type) {
-        case SHOW_PAGE:
-            return state;
+        case LOAD_PAGES:
+            const { type } = action.payload.user;
+            const newState = Object.assign({}, state);
+            newState.items = [
+                {
+                    text: "Companies",
+                    page: "dashboard/companies"
+                }
+            ]
+            switch (type) {
+                case "user":
+                    break;
+                case "manager":
+                    break;
+            }
+            return newState;
         default:
             return state;
     }
