0616b1e575ced6d6428232384a51e14c0762110a
yael <yael19961@gmail.com>
2019-12-13 17:23:45 +0200

    empty board controller

---
+++ server/src/api/controllers/constraintsController.ts
@@ -0,0 +1,23 @@
+import { Router } from "express";
+import { EmptyMonthBord } from "../../utiles/bordCreator/emptyMonthBord";
+import { Month } from "../../mongo/models/concreteBoard";
+import { models } from "../../mongo/Connection";
+
+const router = Router();
+
+router.post("/emptyBoard", async (req, res) => {
+  try {
+    const bord = await models.board.findById(req.query.board);
+    const month = req.body.month as Month;
+    const emptyBoard = new EmptyMonthBord(month, bord.boardSettings);
+    res
+      .status(200)
+      .json(emptyBoard)
+      .end();
+  } catch (error) {
+    res
+      .status(400)
+      .json({ error: error.message })
+      .end();
+  }
+});
